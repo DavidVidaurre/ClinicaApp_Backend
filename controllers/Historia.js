2a60b5c12f1f88a4b1abafa61aa53c1b13670cba
Daniel <=>
2021-10-04 19:37:29 -0500

    Correccion en Historia

--- controllers/Historia.js
+++ controllers/Historia.js
@@ -1,76 +1,89 @@
 const Historia = require('../models/Historia');
-const {validarDNI} = require('../functions/validaciones.js');
-const {validarNombre} = require('../functions/validaciones.js');
+const { validarDNI } = require('../functions/validaciones.js');
+const { validarNombre } = require('../functions/validaciones.js');
 
-const CrearHistoria= async (req, res) => {
-        const {dni_paciente, nombres_paciente,fecha_nac, lugar_nac, direccion,nombre_madre, ocupacion_madre, telefono_madre, nombre_padre, ocupacion_padre, telefono_padre, numero_hijo, referencia} = req.body;
-        try {
-            let historia= await Historia.findOne({ dni_paciente});
-            if (historia) {
-                return res.status(400).json({
-                    ok: false,
-                    msg: 'Ya existe un Historiacon este DNI',
-                });
-            }
-            
-            const DNIvalido = await validarDNI(dni_paciente);
-            if(!DNIvalido){
-              return res.status(400).json({
-                ok: false,
-                msg: 'DNI incorrecto',
-              });
-            }
-            const NOMBREvalido = await validarNombre(nombres_paciente);
-            if(!NOMBREvalido){
-              return res.status(400).json({
-                ok: false,
-                msg: 'Nombre incorrecto',
-              });
-            }
+const CrearHistoria = async (req, res) => {
+	const {
+		dni_paciente,
+		nombres_paciente,
+		fecha_nac,
+		lugar_nac,
+		direccion,
+		nombre_madre,
+		ocupacion_madre,
+		telefono_madre,
+		nombre_padre,
+		ocupacion_padre,
+		telefono_padre,
+		numero_hijo,
+		referencia,
+	} = req.body;
+	try {
+		let historia = await Historia.findOne({ dni_paciente });
+		if (historia) {
+			return res.status(400).json({
+				ok: false,
+				msg: 'Ya existe un Historiacon este DNI',
+			});
+		}
 
+		const DNIvalido = await validarDNI(dni_paciente);
+		if (!DNIvalido) {
+			return res.status(400).json({
+				ok: false,
+				msg: 'DNI incorrecto',
+			});
+		}
+		const NOMBREvalido = await validarNombre(nombres_paciente);
+		if (!NOMBREvalido) {
+			return res.status(400).json({
+				ok: false,
+				msg: 'Nombre incorrecto',
+			});
+		}
 
-            const LUGARNACvalido = await validarNombre(lugar_nac);
-            if(!LUGARNACvalido){
-              return res.status(400).json({
-                ok: false,
-                msg: 'Apellido Materno incorrecto',
-              });
-            }
-            const NOMBREMADREvalido = await validarNombre(nombre_madre);
-            if(!NOMBREMADREvalido){
-              return res.status(400).json({
-                ok: false,
-                msg: 'Nombre incorrecto',
-              });
-            }
-            const NOMBREPADREvalido = await validarNombre(nombre_padre);
-            if(!NOMBREPADREvalido){
-              return res.status(400).json({
-                ok: false,
-                msg: 'Nombre incorrecto',
-              });
-            }
-            historia= new Historia(req.body);
-            
-            await historia.save();
-            
-            res.status(201).json({
-                ok: true,
-                historia: historia,
-            });
-        } catch (error) {
-            console.log('Error: ' + error.toString());
-            res.status(500).json({
-                ok: false,
-                msg: 'Por favor hable con el administrador',
-            });
-        }
- };
+		const LUGARNACvalido = await validarNombre(lugar_nac);
+		if (!LUGARNACvalido) {
+			return res.status(400).json({
+				ok: false,
+				msg: 'Apellido Materno incorrecto',
+			});
+		}
+		const NOMBREMADREvalido = await validarNombre(nombre_madre);
+		if (!NOMBREMADREvalido) {
+			return res.status(400).json({
+				ok: false,
+				msg: 'Nombre incorrecto',
+			});
+		}
+		const NOMBREPADREvalido = await validarNombre(nombre_padre);
+		if (!NOMBREPADREvalido) {
+			return res.status(400).json({
+				ok: false,
+				msg: 'Nombre incorrecto',
+			});
+		}
+		historia = new Historia(req.body);
 
-const ActualizarHistoria= async (req, res = response) => {
+		await historia.save();
+
+		res.status(201).json({
+			ok: true,
+			historia: historia,
+		});
+	} catch (error) {
+		console.log('Error: ' + error.toString());
+		res.status(500).json({
+			ok: false,
+			msg: 'Por favor hable con el administrador',
+		});
+	}
+};
+
+const ActualizarHistoria = async (req, res = response) => {
 	const HistoriaId = req.params.id;
 	try {
-		const Historia= await Historia.findById(HistoriaId);
+		const Historia = await Historia.findById(HistoriaId);
 		if (!Historia) {
 			res.status(404).json({
 				ok: false,
@@ -78,7 +91,7 @@ const ActualizarHistoria= async (req, res = response) => {
 			});
 		}
 
-		const nuevoHistoria= {
+		const nuevoHistoria = {
 			...req.body,
 		};
 
@@ -102,14 +115,13 @@ const ActualizarHistoria= async (req, res = response) => {
 	}
 };
 
+const MostrarHistoria = async (req, res) => {
+	const historia = await Historia.find();
+	return res.json(historia);
+};
 
-const MostrarHistoria= async (req, res) => {
-    const historia= await Historia.find();
-    return res.json(historia);
-}
-    
 module.exports = {
 	CrearHistoria,
-  ActualizarHistoria,
-	MostrarHistoria
-}
+	ActualizarHistoria,
+	MostrarHistoria,
+};
