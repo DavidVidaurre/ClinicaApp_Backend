b6f8269c94981f7175f7090ce521c6c802f219ad
DavidVidaurre <luisdavidmaquenvidaurre@gmail.com>
2022-05-17 23:18:02 -0500

    Se elmiinó el email obligatorio para usuario

--- routes/auth.js
+++ routes/auth.js
@@ -27,7 +27,7 @@ router.post(
 	[
 		//middlewares
 		check('nombre', 'el nombre es obligatorio').not().isEmpty(),
-		check('email', 'El email es obligatorio').isEmail(),
+		// check('email', 'El email es obligatorio').isEmail(),
 		check(
 			'password',
 			'El password debe ser de 6 caracteres en adelante'
